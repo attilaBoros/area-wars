fd9c5cbb3f280fd42ba2abf27f5dd394eae90ec6
Attila Boros <boros.attila17@gmail.com>
2019-08-14 11:34:49 +0200

    basic skeleton structure

---
+++ static/game.js
@@ -0,0 +1,23 @@
+function main() {
+
+    document.getElementById('game-board').addEventListener('keydown', movePlayer);
+        function movePlayer() {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+main();
