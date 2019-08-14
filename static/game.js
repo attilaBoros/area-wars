893d49d5f211e90ddd62fce4372e01948b2c0abc
Attila Boros <boros.attila17@gmail.com>
2019-08-14 14:37:01 +0200

    place players

--- static/game.js
+++ static/game.js
@@ -1,10 +1,7 @@
 function main() {
-
+    document.querySelector('[data-coordinate-x="0"][data-coordinate-y="34"]').classList.add('player1');
+    document.querySelector('[data-coordinate-x="129"][data-coordinate-y="34"]').classList.add('player2');
     document.getElementById('game-board').addEventListener('keydown', movePlayer);
-        function movePlayer() {
-
-        }
-
 
 
 
@@ -19,5 +16,7 @@ function main() {
 
 
 }
+
+
 
 main();
