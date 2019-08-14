9460c754bd6b542c0154d9b7fc5a037133592dec
gergomahler <mahlergergo@gmail.com>
2019-08-14 14:21:56 +0200

    created a win checker (not tested)

--- static/game.js
+++ static/game.js
@@ -13,11 +13,23 @@ function main() {
 
 
 
+    function getAreaCells(area) {
+        return document.getElementsByClassName(area);
+    }
 
-
-
-
-
+    function checkGameOver(player, playerArea, opponentArea) {
+        let boardSize = getAreaCells('game-board');
+        let playerAreaCells = getAreaCells(playerArea);
+        let opponentAreaCells = getAreaCells(opponentArea);
+        if (playerAreaCells.length > boardSize.length/2) {
+            return true
+        }
+        for (cell of opponentAreaCells) {
+            if (player == cell) {
+                return true
+            }
+        }
+    }
 }
 
 main();
