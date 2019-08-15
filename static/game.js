f5cad2ab427bdbddddb263603e8e8c95779de12f
gergomahler <mahlergergo@gmail.com>
2019-08-15 13:28:21 +0200

    WIP player1 collision

--- static/game.js
+++ static/game.js
@@ -1,34 +1,22 @@
+function player1Collision() {
+    let player1 = document.querySelector('player1');
+    let redLine = [];
+    if (document.querySelector('game-board').classList.length === 2) {
+        redLine = document.querySelectorAll("[data-coordinate-x][data-coordinate-y]").classList.contains('red')
+    }
+    for (cell in redLine) {
+        if (player1 === cell) {
+            return true
+        }
+    }
+}
+
+
 function main() {
     document.querySelector('[data-coordinate-x="0"][data-coordinate-y="34"]').classList.add('player1');
     document.querySelector('[data-coordinate-x="129"][data-coordinate-y="34"]').classList.add('player2');
-    document.getElementById('game-board').addEventListener('keydown', movePlayer);
+    //document.getElementById('game-board').addEventListener('keydown', movePlayer);
 
-
-
-
-
-
-
-
-
-
-    function getAreaCells(area) {
-        return document.getElementsByClassName(area);
-    }
-
-    function checkGameOver(player, playerArea, opponentArea) {
-        let boardSize = getAreaCells('game-board');
-        let playerAreaCells = getAreaCells(playerArea);
-        let opponentAreaCells = getAreaCells(opponentArea);
-        if (playerAreaCells.length > boardSize.length/2) {
-            return true
-        }
-        for (cell of opponentAreaCells) {
-            if (player == cell) {
-                return true
-            }
-        }
-    }
 }
 
 main();
