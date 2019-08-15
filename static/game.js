8c23bd3c84eddfe3b7c81a1ddd0ea9f7947de4c5
gergomahler <mahlergergo@gmail.com>
2019-08-15 16:36:25 +0200

    almost final version

--- static/game.js
+++ static/game.js
@@ -6,10 +6,13 @@ const up = 38;
 const down = 40;
 const left = 37;
 const right = 39;
+
 let direction1 = 'right';
 let direction2 = 'left';
-let redLine = [];
-let blueLine = [];
+
+let set1 = setInterval(movePlayer1_, 70);
+let set2 = setInterval(movePlayer2_,70);
+
 function setDirectionPlayer1(event) {
     console.log(event.keyCode);
     if (event.keyCode === w && direction1 !== 'down') {
@@ -19,11 +22,11 @@ function setDirectionPlayer1(event) {
     } else if (event.keyCode === a && direction1 !== 'right') {
         direction1 = 'left';
     } else if (event.keyCode === d && direction1 !== 'left') {
+
         direction1 = 'right';
-
     }
-}
 
+}
 function setDirectionPlayer2(event) {
     if (event.keyCode === up && direction1 !== 'down') {
         direction2 = 'up';
@@ -34,110 +37,97 @@ function setDirectionPlayer2(event) {
     } else if (event.keyCode === right && direction2 !== 'left') {
         direction2 = 'right'
     }
-}
 
+}
 function movePlayer1_() {
     let player1 = document.querySelector(`.player1`);
     let x = player1.dataset.coordinateX;
     let y = player1.dataset.coordinateY;
-    if (player1Collision() && player2Collision()) {
+    if (player1Collision() && player2Collision() && player1HitWall() && player2HitWall()) {
         if (direction1 === 'right') {
             player1.classList.add('red');
             player1.classList.remove('player1');
-            let cellCoordinates = [x, y];
-            redLine.push(cellCoordinates);
             document.querySelector(`[data-coordinate-x="${parseInt(x) + 1}"][data-coordinate-y="${y}"]`).classList.add('player1');
         } else if (direction1 === 'left') {
             player1.classList.add('red');
             player1.classList.remove('player1');
-            let cellCoordinates = [x, y];
-            redLine.push(cellCoordinates);
             document.querySelector(`[data-coordinate-x="${parseInt(x) - 1}"][data-coordinate-y="${y}"]`).classList.add('player1');
         } else if (direction1 === 'up') {
             player1.classList.add('red');
             player1.classList.remove('player1');
-            let cellCoordinates = [x, y];
-            redLine.push(cellCoordinates);
             document.querySelector(`[data-coordinate-x="${x}"][data-coordinate-y="${parseInt(y) - 1}"]`).classList.add('player1');
         } else if (direction1 === 'down') {
             player1.classList.add('red');
             player1.classList.remove('player1');
-            let cellCoordinates = [x, y];
-            redLine.push(cellCoordinates);
             document.querySelector(`[data-coordinate-x="${x}"][data-coordinate-y="${parseInt(y) + 1}"]`).classList.add('player1');
         }
     } else {
-        return 0;
-    }
+        clearInterval(set1);
+        clearInterval(set2);
 
+    }
 }
 function movePlayer2_() {
     let player2 = document.querySelector(`.player2`);
     let x = player2.dataset.coordinateX;
     let y = player2.dataset.coordinateY;
-    if (player1Collision() && player2Collision()) {
+    if (player1Collision() && player2Collision() && player1HitWall() && player2HitWall()) {
         if (direction2 === 'right') {
-           player2.classList.add('blue');
-           player2.classList.remove('player2');
-           let cellCoordinates = [x, y];
-           blueLine.push(cellCoordinates);
-           document.querySelector(`[data-coordinate-x="${parseInt(x) + 1}"][data-coordinate-y="${y}"]`).classList.add('player2');
+            player2.classList.add('blue');
+            player2.classList.remove('player2');
+            document.querySelector(`[data-coordinate-x="${parseInt(x) + 1}"][data-coordinate-y="${y}"]`).classList.add('player2');
         } else if (direction2 === 'left') {
-           player2.classList.add('blue');
-           player2.classList.remove('player2');
-           let cellCoordinates = [x, y];
-           blueLine.push(cellCoordinates);
-           document.querySelector(`[data-coordinate-x="${parseInt(x) - 1}"][data-coordinate-y="${y}"]`).classList.add('player2');
+            player2.classList.add('blue');
+            player2.classList.remove('player2');
+            document.querySelector(`[data-coordinate-x="${parseInt(x) - 1}"][data-coordinate-y="${y}"]`).classList.add('player2');
         } else if (direction2 === 'up') {
-           player2.classList.add('blue');
-           player2.classList.remove('player2');
-           let cellCoordinates = [x, y];
-           blueLine.push(cellCoordinates);
-           document.querySelector(`[data-coordinate-x="${x}"][data-coordinate-y="${parseInt(y) - 1}"]`).classList.add('player2');
+            player2.classList.add('blue');
+            player2.classList.remove('player2');
+            document.querySelector(`[data-coordinate-x="${x}"][data-coordinate-y="${parseInt(y) - 1}"]`).classList.add('player2');
         } else if (direction2 === 'down') {
-           player2.classList.add('blue');
-           player2.classList.remove('player2');
-           let cellCoordinates = [x, y];
-           blueLine.push(cellCoordinates);
-           document.querySelector(`[data-coordinate-x="${x}"][data-coordinate-y="${parseInt(y) + 1}"]`).classList.add('player2');
+            player2.classList.add('blue');
+            player2.classList.remove('player2');
+            document.querySelector(`[data-coordinate-x="${x}"][data-coordinate-y="${parseInt(y) + 1}"]`).classList.add('player2');
         }
     } else {
-        return 0;
+        clearInterval(set1);
+        clearInterval(set2);
     }
+
+
 }
+function player1HitWall() {
+    let player1 = document.querySelector('.player1');
+    return !(player1.dataset.coordinateX === '0' || player1.dataset.coordinateX === '129' || player1.dataset.coordinateY === '0' || player1.dataset.coordinateY === '69');
 
 
-
-function canMove(direction, x, y) {
-    return true;
 }
+function player2HitWall() {
+    let player2 = document.querySelector('.player2');
+    return !(player2.dataset.coordinateX === '0' || player2.dataset.coordinateX === '129' || player2.dataset.coordinateY === '0' || player2.dataset.coordinateY === '69');
 
 
-
-
-
+}
 function player1Collision() {
     let player1 = document.querySelector('.player1');
-    return !player1.classList.contains('blue');
+    return !player1.classList.contains('blue') && !player1.classList.contains('red');
+
+
 }
-
-
 function player2Collision() {
     let player2 = document.querySelector('.player2');
-    return !player2.classList.contains('red')
+    return !player2.classList.contains('red') && !player2.classList.contains('blue');
+
+
 }
 
-
 function main() {
-
-    document.querySelector('[data-coordinate-x="0"][data-coordinate-y="34"]').classList.add('player1');
-    document.querySelector('[data-coordinate-x="129"][data-coordinate-y="34"]').classList.add('player2');
+    document.querySelector('[data-coordinate-x="1"][data-coordinate-y="34"]').classList.add('player1');
+    document.querySelector('[data-coordinate-x="128"][data-coordinate-y="34"]').classList.add('player2');
     let event = window.addEventListener('keydown', setDirectionPlayer1);
+
+
     let event2 = window.addEventListener('keydown', setDirectionPlayer2);
-
-
-    let set = setInterval(movePlayer1_, 200);
-    let set2 = setInterval(movePlayer2_,200);
     console.log('hello');
 
 
